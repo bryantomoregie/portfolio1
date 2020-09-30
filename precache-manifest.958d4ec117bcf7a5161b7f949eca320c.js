d1e41ad3620c5ba47e93ee92365371c0711f44a6
bryantomoregie <unclebryant@gmail.com>
2020-09-29 19:03:40 -0500

    Updates

---
+++ precache-manifest.958d4ec117bcf7a5161b7f949eca320c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30ba01de39fa4602eeaf69240c0d231",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "7423d7ec3b179e9ee6df",
+    "url": "/portfolio1/static/css/main.a976a22e.chunk.css"
+  },
+  {
+    "revision": "fef00cbb155d47e6c9f7",
+    "url": "/portfolio1/static/js/2.47ca7890.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio1/static/js/2.47ca7890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7423d7ec3b179e9ee6df",
+    "url": "/portfolio1/static/js/main.59642579.chunk.js"
+  },
+  {
+    "revision": "43cb86accc6e0f3720db",
+    "url": "/portfolio1/static/js/runtime-main.1ca99ad0.js"
+  },
+  {
+    "revision": "46bdf5f1bc09d90626f848408a52f72c",
+    "url": "/portfolio1/static/media/itni.46bdf5f1.png"
+  },
+  {
+    "revision": "3f02cf0b3ec2272a5efa0aa6592e02e9",
+    "url": "/portfolio1/static/media/mountains.3f02cf0b.jpeg"
+  },
+  {
+    "revision": "77e12aa73514a9bb5f8a104fdefb141b",
+    "url": "/portfolio1/static/media/nature.77e12aa7.jpg"
+  },
+  {
+    "revision": "2b19e7f79dfb2c31a938c2b003c3f78e",
+    "url": "/portfolio1/static/media/portfolio.2b19e7f7.png"
+  },
+  {
+    "revision": "df911c433020e73e1e4f969c3dc0d1d7",
+    "url": "/portfolio1/static/media/shoe-palace.df911c43.png"
+  }
+]);
